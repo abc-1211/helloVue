664214b2fb1046b400e0b15a7ecc8b614cc3f90d
abc1211 <chengkwanyu@yahoo.com.hk>
2022-09-23 17:16:05 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "/helloVue/dist" : "/",
+    publicPath: process.env.NODE_ENV === "production" ? "/helloVue/dist/index.html" : "/",
 };
